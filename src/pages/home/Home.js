b8e1c254c2428b09a3aaddf9b2a0c287d8cab3b6
Mirte Houwing <mirtehouwing@Mirtes-MacBook-Pro.local>
2022-04-15 20:50:56 +0200

    folder structure

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(props) {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/signup/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css';
+
+function SignUp(props) {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SignUp;
